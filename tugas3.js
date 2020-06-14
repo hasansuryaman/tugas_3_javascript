5c0e4f026c3c420a314bee7e569a93a434bb4a56
hasansuryaman <hasan.suryaman@outlook.com>
2020-06-14 21:04:16 +0700

    Tugas 3 Javascript

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+// Function dengan nilai balik (return)
+function ubah_nilai(x){
+    // Tampil nilai sebelum
+    console.log(x)
+    // Ubah nilai dari variabel lalu dikembalikan (return)
+    return x + 333
+}
+
+// Kirim nilai kedalam function (666)
+// Tampil nilai sesudah
+console.log(ubah_nilai(666))
